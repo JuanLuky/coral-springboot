e7d672bc6ff3b4603baf3f063df479b986c3cd39
Juan Carlos <juandev33@gmail.com>
2024-06-02 21:49:36 -0300

    feet: sistema de login e signup criado com sucesso, com spring security e tokens.

--- src/main/java/com/ecommerce/coral/infra/security/SecurityConfig.java
+++ src/main/java/com/ecommerce/coral/infra/security/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/api/v1/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
